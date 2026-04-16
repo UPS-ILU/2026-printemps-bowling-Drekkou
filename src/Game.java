b65812125a9359eec31728c6cc3be6b5087a0eca
Louis Faliu <fll5009a@ensfsi.onmicrosoft.com>
2026-04-16 18:45:39 +0200

    ITER_1

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,18 @@
 
 public class Game {
+	int score;
+	
+	
+	
+	public Game() {
+		this.score =0;
+	}
 
+	public void roll(int nombre) {
+		this.score+=nombre;
+	}
+	
+	public int score() {
+		return this.score;
+	}
 }
--- test/GameTest.java
+++ test/GameTest.java
@@ -1,12 +1,41 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class GameTest {
-
+	Game game;
+	
+	@BeforeEach
+	public void init() {
+		this.game = new Game();
+	}
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void entréeVide() {
+		assertEquals(game.score(), 0);
+	}
+	
+	@Test
+	void vingtRollZero() {
+		for(int i=0; i<20;i++)game.roll(0);
+		
+		assertEquals(game.score(), 0);
+	}
+	
+	@Test
+	void vingtRollUn() {
+		for(int i=0; i<20;i++)game.roll(1);
+		
+		assertEquals(game.score(), 20);
+	}
+	
+	@Test
+	void dixRollUnDixRollDeux(){
+		for(int i=0; i<10;i++)game.roll(1);
+		for(int i=0; i<10;i++)game.roll(2);
+		
+		assertEquals(game.score(), 30);
 	}
 
 }
